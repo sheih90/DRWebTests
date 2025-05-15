f9ab98b00da6ffd41fa14422610a4082a22dbd5a
Дамир <sheih90@mail.ru>
2025-05-15 19:08:03 +0300

    Повторил код из урока, добавил тест test_empty_password

---
+++ tests/LoginTests.py
@@ -0,0 +1,20 @@
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+
+BASE_URL = "https://ok.ru/"
+EMPTY_LOGIN_ERROR = "Введите логин"
+EMPTY_PASSWORD_ERROR = "Введите пароль"
+
+def test_empty_login_and_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.click_login()
+    assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
+
+def test_empty_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.send_login()
+    LoginPage.click_login()
+    assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
